890fc554c153955eb9bbea4fbed96de47462fb89
Barbara790 <barbara2.dossantos@gmail.com>
2021-05-28 20:11:01 -0300

    alterações blog_pessoal

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 
@@ -32,6 +35,11 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)//indica para o jpa hibernate que estamos trabalhando com tempo
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public long getId() {
 		return id;
 	}
@@ -57,7 +65,11 @@ public class Postagem {
 		this.date = date;
 	}
 	
-	
-	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
